51e88b5c5f8fa37b897a2365aa1e735e286a1f8d
OTS <88117721+orgtecsalutem@users.noreply.github.com>
2021-10-14 12:04:30 -0300

    Update links.js

--- src/utils/links.js
+++ src/utils/links.js
@@ -1,22 +1,10 @@
 export const navLinks = [
   {
     path: "/",
-    name: "INÍCIO",
-  },
-  {
-    path: "/",
     name: "INSTITUCIONAL",
   },
   {
     path: "/",
     name: "PUBLICAÇÕES",
-  },
-  {
-    path: "/",
-    name: "PROGRAMAS",
-  },
-  {
-    path: "/",
-    name: "EVENTOS",
   },
 ];
